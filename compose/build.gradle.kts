a90bc7dd8d30c049863d40a85f0119e76dffb686
andylamax <andylamax@programmer.net>
2023-07-23 02:12:03 +0300

    Before auto merging main

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -1,16 +1,13 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 
 plugins {
-    id("com.android.library")
     kotlin("multiplatform")
     id("org.jetbrains.compose")
 }
 
 kotlin {
-    android()
-    jvm()
-    if(Targeting.JS) js(IR) { browser() }
+    if (Targeting.JVM) jvm { targetJava() }
+    if (Targeting.JS) js(IR) { browser() }
 
     sourceSets {
         val commonMain by getting {
@@ -21,49 +18,12 @@ kotlin {
             }
         }
 
-        val androidMain by getting {
-            dependencies {
-                api(compose.foundation)
-                api(compose.uiTooling)
-                api(compose.preview)
-            }
-        }
-
         val jvmMain by getting {
             dependencies {
                 api(compose.uiTooling)
                 api(compose.preview)
                 api(compose.desktop.currentOs)
             }
-        }
-    }
-}
-
-android {
-    compileSdk = 32
-
-    namespace = "tz.co.asoft.aesthetics.compose"
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 31
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    buildFeatures {
-        buildConfig = false
-        compose = true
-    }
-
-    sourceSets {
-        getByName("main") {
-            manifest.srcFile("src/androidMain/AndroidManifest.xml")
-//            res.srcDirs("src/androidMain/res", "src/commonMain/resources")
-//        }
         }
     }
 }
